4886efd261940a2ee5e87ae2754a12165333170f
Valencius <valenciusxr@gmail.com>
2022-08-20 17:14:22 +0700

    New

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <h1 class="text-regal text-xl p-12 font-bold underline m-3 bg-red-200 hover:bg-red-500">
+    Hello world!
+  </h1>
+</body>
+</html>
