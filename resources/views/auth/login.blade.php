c3138bfd467714cca80f3d03903beeb5818d0df1
Bro3.14 <ln060303@gmail.com>
2022-08-31 14:23:53 +0700

    redesigned login page and added new font

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,18 +1,22 @@
 @extends('layouts.app')
+<img src="{{ asset('asset/ellipse.png')}}" id="ellipse2">
+<img src="{{ asset('asset/ellipse.png')}}" id="ellipse1"> 
+<img src="{{ asset('asset/triangle1.png')}}" id="triangle1">
+<img src="{{ asset('asset/triangle2.png')}}" id="triangle2">
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+            <div class="card" id="card-box">
+                <div class="card-header" id="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="email" id="email-title" class="col-md-4 col-form-label text-md-end" >{{ __('Email') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -26,7 +30,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                            <label for="password" id="password-title" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -53,7 +57,7 @@
 
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary" id="submit-btn">
                                     {{ __('Login') }}
                                 </button>
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link rel="stylesheet" href="./css/home.css"> {{-- css home --}}
     <link rel="stylesheet" href="{{ asset('css/createPeserta.css') }}">
+    <link rel="stylesheet" href="./css/login.css">
     
     {{-- tambahin disini buat css lainnya --}}
     {{-- boleh liat home.css sama variables.scss dulu soalnya ada brp yg gw set default--}}
