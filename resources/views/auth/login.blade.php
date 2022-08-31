196ec3859a78bec751ae04024b4b9778a56caa5a
Bro3.14 <ln060303@gmail.com>
2022-08-31 17:28:00 +0700

    redesigned register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,7 +62,7 @@
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    <a class="btn btn-link" id="forgot-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
