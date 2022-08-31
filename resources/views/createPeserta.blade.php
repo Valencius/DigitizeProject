07665a4676813e00f8278f1547a7c2d9c9c0699b
averine08 <averinanurdin08@gmail.com>
2022-08-31 16:30:13 +0700

    edit createPeserta.css

--- resources/views/createPeserta.blade.php
+++ resources/views/createPeserta.blade.php
@@ -2,47 +2,48 @@
 
 @section('content')
 
-<div class="containerCreate container mt-5 mb-5 " >
-  {{-- <div class="containerCreate"> --}}
-    <h2>Create Peserta</h2>
+<div class="container mt-5 mb-5" id="create-peserta-box" >
+    <h2 class="py-3">Create Peserta</h2>
     <hr>
-
-    <form action="{{ route('peserta.store') }}" enctype="multipart/form-data" method="POST" id="formCreate" >
-        @csrf
-
-        <div class="mb-3 mx-5">
-            <label for="picture" class="form-label">Choose Picture</label>
-            <input class="form-control" type="file" name="picture" id="picture">
-          </div>
-
-        <div class="mb-3 mx-5">
-            <label for="Nama" class="form-label">Nama</label>
-            <input type="text" class="form-control" name="Nama" id="Nama" placeholder="Enter Nama">
-          </div>
+    <div class="form-box">
+      <form action="{{ route('peserta.store') }}" enctype="multipart/form-data" method="POST" id="formCreate" >
+          @csrf
 
           <div class="mb-3 mx-5">
-            <label for="creator" class="form-label">Creator</label>
-            <input type="text" class="form-control" name="creator" id="creator" placeholder="Enter creator">
-          </div>
+              <label for="picture" class="form-label">Choose Picture</label>
+              <input class="form-control" type="file" name="picture" id="picture">
+            </div>
 
           <div class="mb-3 mx-5">
-            <label for="category" class="form-label">Category Lomba</label>
-            <select class="form-select" name="category" id="category">
-              <option value="Drawing">Drawing</option>
-              <option value="Singing">Singing</option>
-              <option value="Dancing">Dancing</option>
-            </select>
-          </div>
+              <label for="Nama" class="form-label">Name</label>
+              <input type="text" class="form-control" name="Nama" id="Nama" placeholder="Enter Name">
+            </div>
 
-          <div class="mb-3 mx-5">
-            <label for="desc" class="form-label">Description</label>
-            <textarea class="form-control" name="desc" id="desc" placeholder="Enter Description"></textarea>
-          </div>
+            <div class="mb-3 mx-5">
+              <label for="creator" class="form-label">Creator</label>
+              <input type="text" class="form-control" name="creator" id="creator" placeholder="Enter Creator">
+            </div>
 
-          <button type="submit" class="btn btn-primary">Create Peserta</button>
+            <div class="mb-3 mx-5">
+              <label for="category" class="form-label">Competition Category</label>
+              <select class="form-select" name="category" id="category">
+                {{-- <option selected hidden value="" disabled >Choose a competition</option> --}}
+                <option value="Drawing">Drawing</option>
+                <option value="Singing">Singing</option>
+                <option value="Dancing">Dancing</option>
+              </select>
+            </div>
 
-    </form>
-  {{-- </div> --}}
+            <div class="mb-3 mx-5">
+              <label for="desc" class="form-label">Description</label>
+              <textarea class="form-control" name="desc" id="desc" placeholder="Enter Description"></textarea>
+            </div>
+
+            
+          </form>
+          <button type="submit" class="btn btn-primary mx-5" id="create-btn">Create Peserta</button>
+    </div>
+  </div>
 </div>
 
 
