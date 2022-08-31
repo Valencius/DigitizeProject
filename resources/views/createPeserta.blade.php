71965e9133a58c1523e92c28a5ae139dbb7df33a
averine08 <averinanurdin08@gmail.com>
2022-08-31 14:26:00 +0700

    edit createPeserta.css

--- resources/views/createPeserta.blade.php
+++ resources/views/createPeserta.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="container mt-5 mb-5" id="containerCreate" >
+<div class="containerCreate container mt-5 mb-5 " >
   {{-- <div class="containerCreate"> --}}
     <h2>Create Peserta</h2>
     <hr>
@@ -10,22 +10,22 @@
     <form action="{{ route('peserta.store') }}" enctype="multipart/form-data" method="POST" id="formCreate" >
         @csrf
 
-        <div class="mb-3">
+        <div class="mb-3 mx-5">
             <label for="picture" class="form-label">Choose Picture</label>
             <input class="form-control" type="file" name="picture" id="picture">
           </div>
 
-        <div class="mb-3">
+        <div class="mb-3 mx-5">
             <label for="Nama" class="form-label">Nama</label>
             <input type="text" class="form-control" name="Nama" id="Nama" placeholder="Enter Nama">
           </div>
 
-          <div class="mb-3">
+          <div class="mb-3 mx-5">
             <label for="creator" class="form-label">Creator</label>
             <input type="text" class="form-control" name="creator" id="creator" placeholder="Enter creator">
           </div>
 
-          <div class="mb-3">
+          <div class="mb-3 mx-5">
             <label for="category" class="form-label">Category Lomba</label>
             <select class="form-select" name="category" id="category">
               <option value="Drawing">Drawing</option>
@@ -34,7 +34,7 @@
             </select>
           </div>
 
-          <div class="mb-3">
+          <div class="mb-3 mx-5">
             <label for="desc" class="form-label">Description</label>
             <textarea class="form-control" name="desc" id="desc" placeholder="Enter Description"></textarea>
           </div>
