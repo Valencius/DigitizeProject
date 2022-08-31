d537ad483f8ac440a1cd481cf7d4aae62a954c0f
ClaudyaSalim <88886593+ClaudyaSalim@users.noreply.github.com>
2022-08-31 20:07:12 +0700

    Singing Competition

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -40,17 +40,16 @@
             <div class="drawing-comp">
                 <div id="drawing-heading">
                     <h2>Drawing Competition</h2>
-                    <a id="see-all-link">See all</a>
+                    <a id="see-all-link" href="#">See all</a>
                 </div>
                 <div class="list-home">
                     <?php $count = 0; ?>
                     @foreach ($pesertas as $peserta)
                         <?php if($count == 3) break; ?>
                         @if($peserta->category === "Drawing")
-                            <div class="card-drawing" style="background-image: linear-gradient(36.07deg, rgba(0, 0, 0, 0.6) 3.45%, rgba(0, 0, 0, 0) 97.47%), url({{ asset('files/'.$peserta->picture) }})">
-                        
-                                {{-- <img src="{{ asset('files/'.$peserta->picture) }}" class="card-img-top" alt="..."> --}}
-                                <div class="card-body-drawing">
+                            <div class="card-dancing" style="radial-gradient(127.68% 13497.49% at 1.6% 8.93%, rgba(255, 255, 255, 0.37) 0%, rgba(255, 255, 255, 0) 100%)">
+                                
+                                <div class="card-body-dancing">
                                     <div class="card-detail">
                                         <h4 class="card-title">{{ $peserta->creator }}</h4>
                                         <h5 class="card-title">{{ $peserta->Nama }}</h5>
@@ -64,6 +63,58 @@
                 </div>
             </div>
             
+            <div class="singing-comp">
+                <div id="singing-heading">
+                    <h2>Singing Competition</h2>
+                    <a id="see-all-link" href="#">See all</a>
+                </div>
+                <div class="list-home-singing">
+                    <?php $count = 0; ?>
+                    @foreach ($pesertas as $peserta)
+                        <?php if($count == 3) break; ?>
+                        @if($peserta->category === "Singing")
+                            <div class="card-singing" style="background: radial-gradient(127.68% 13497.49% at 1.6% 8.93%, rgba(255, 255, 255, 0.37) 0%, rgba(255, 255, 255, 0) 100%)">
+                                <div class="card-body-singing">
+                                    <div class="card-detail">
+                                        <h4 class="card-title">{{ $peserta->creator }}</h4>
+                                        <h5 class="card-title">{{ $peserta->Nama }}</h5>
+                                    </div>
+                                    <audio controls style="width: 100%">
+                                        <source src="{{ asset('files/'.$peserta->picture) }}">
+                                    </audio>
+                                    <a href="{{ route('peserta.show',$peserta->id) }}" class="btn btn-primary" id="vote">Vote</a>
+                                </div>
+                            </div>
+                        <?php $count++; ?>
+                        @endif
+                    @endforeach
+                </div>
+            </div>
+
+            <div class="dancing-comp">
+                <div id="dancing-heading">
+                    <h2>Dancing Competition</h2>
+                    <a id="see-all-link" href="#">See all</a>
+                </div>
+                <div class="list-home">
+                    <?php $count = 0; ?>
+                    @foreach ($pesertas as $peserta)
+                        <?php if($count == 3) break; ?>
+                        @if($peserta->category === "Dancing")
+                            <div class="card-drawing" style="background: url({{ asset('files/'.$peserta->picture) }})">
+                                <div class="card-body-drawing">
+                                    <div class="card-detail">
+                                        <h4 class="card-title">{{ $peserta->creator }}</h4>
+                                        <h5 class="card-title">{{ $peserta->Nama }}</h5>
+                                    </div>
+                                    <a href="{{ route('peserta.show',$peserta->id) }}" class="btn btn-primary" id="vote">Vote</a>
+                                </div>
+                            </div>
+                        <?php $count++; ?>
+                        @endif
+                    @endforeach
+                </div>
+            </div>
         </div>
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,7 +41,7 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link active" aria-current="page" href="/">Home</a>
                          </li>
                          <li class="nav-item">
                             <a class="nav-link" href="/#competitionHeading">Competition</a>
